dea88d310038f9191b27960edf44cf1ed29568d7
Duco <git@ducode.org>
2023-07-09 20:40:11 +0200

    Added logo and some other CSS fixes

--- src/template-parts/footer/copyright-footer.php
+++ src/template-parts/footer/copyright-footer.php
@@ -1,9 +1,10 @@
 <?php
+$site_url      = get_site_url();
 $site_name = get_bloginfo( 'name' );
 $year      = date( 'Y' );
 ?>
 <div class="copyright-footer">
     <div class="container inner">
-        &copy; <?php echo $year; ?> <?php echo $site_name; ?>
+        &copy; <?php echo $year; ?> <a href="<?php echo $site_url; ?>"><?php echo $site_name; ?></a>
     </div>
 </div>
--- src/template-parts/header/navbar.php
+++ src/template-parts/header/navbar.php
@@ -1,14 +1,32 @@
-<!-- TODO logo -->
+<?php
+$site_url      = get_site_url();
+$template_path = get_template_directory_uri();
+$site_name     = get_bloginfo( 'name' );
+?>
 <div class="main-menu">
     <div class="container inner">
-        <div class="main-menu-mobile-bar" id="main-menu-mobile-bar"><i class="bi bi-list menu-icon"></i>
-            <span>Menu</span>
+        <div class="main-menu-mobile-bar" id="main-menu-mobile-bar">
+            <div>
+                <i class="bi bi-list menu-icon"></i>
+                <span>Menu</span>
+            </div>
+            <div class="logo">
+                <a href="<?php echo $site_url; ?>">
+                    <img src="<?php echo $template_path; ?>/public/static/img/logo.png"
+                         alt="<?php echo $site_name; ?>"/>
+                </a>
+            </div>
         </div>
-	    <?php wp_nav_menu( array(
-		    "theme_location"  => "primary",
-		    "menu_class"      => "main-menu-ul",
-		    "container_class" => "menu",
-		    "container_id"    => "main-menu-container"
-	    ) ); ?>
+        <div class="logo">
+            <a href="<?php echo $site_url; ?>">
+                <img src="<?php echo $template_path; ?>/public/static/img/logo.png" alt="<?php echo $site_name; ?>"/>
+            </a>
+        </div>
+		<?php wp_nav_menu( array(
+			"theme_location"  => "primary",
+			"menu_class"      => "main-menu-ul",
+			"container_class" => "menu",
+			"container_id"    => "main-menu-container"
+		) ); ?>
     </div>
 </div>
