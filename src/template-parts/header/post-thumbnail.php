5161ceb5276335d52187cf0f54e460db1d3bcf9e
Duco <git@ducode.org>
2023-07-15 13:57:10 +0200

    Post thumbnail fix

--- src/template-parts/header/post-thumbnail.php
+++ src/template-parts/header/post-thumbnail.php
@@ -1,8 +1,11 @@
 <?php
-$featured_image = has_post_thumbnail() ?
-	wp_get_attachment_image_src( get_post_thumbnail_id(), "medium_full" )[0] : "";
-if ( $featured_image ):
-?>
-<header class="post-thumbnail" style="background-image: url(<?php echo $featured_image; ?>);"></header>
-<?php
+if ( ! is_archive() && ! is_front_page() ):
+	$post = get_post();
+	$featured_image = has_post_thumbnail() ?
+		wp_get_attachment_image_src( get_post_thumbnail_id(), "medium_full" )[0] : "";
+	if ( $featured_image ):
+		?>
+        <header class="post-thumbnail" style="background-image: url(<?php echo $featured_image; ?>);"></header>
+	<?php
+	endif;
 endif;
