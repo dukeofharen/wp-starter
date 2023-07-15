c955d801fa99d096b8a833854fcc4397decfcefa
Duco <git@ducode.org>
2023-07-15 15:55:34 +0200

    CSS fixes

--- src/assets/scripts/components/navbar.ts
+++ src/assets/scripts/components/navbar.ts
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 $(() => {
     // Hookup listeners for the main menu mobile bar.
-    $('#main-menu-mobile-bar').on("click", () => {
+    $('.menu-button-wrapper').on("click", () => {
         const $mainMenuContainer = $('#main-menu-container');
         const duration = 200;
         if ($mainMenuContainer.is(':visible')) {
