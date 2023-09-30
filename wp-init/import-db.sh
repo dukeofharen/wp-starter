705cabd988d8cf6c2768b5abcaf96d4667c9b996
Duco <git@ducode.org>
2023-09-30 19:29:46 +0200

    Check if DB already imported

--- wp-init/import-db.sh
+++ wp-init/import-db.sh
@@ -3,7 +3,14 @@ set -eu
 echo "Running DB import"
 sleep 10
 RESTORE_PATH="/etc/wp-starter/restore/dump.sql"
+CHECK_FILE="/etc/clidata/sql_dump_imported"
+if [ -f "$CHECK_FILE" ]; then
+  echo "File $RESTORE_PATH already imported."
+  exit 0
+fi
+
 if [ -f "$RESTORE_PATH" ]; then
   echo "Path $RESTORE_PATH found. Now running MySQL import..."
   mysql -h mysql --port 3306 -u root -p$MYSQL_ROOT_PASSWORD --database $MYSQL_DATABASE < $RESTORE_PATH
+  touch $CHECK_FILE
 fi
